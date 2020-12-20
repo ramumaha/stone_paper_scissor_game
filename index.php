b0cc7efda3d2919111d08a849594141bed602a39
ramumaha <ramumaha123@gmail.com>
2020-12-20 06:44:30 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kanyalakshmi</title>
+</head>
+<body>
+    <h1>
+        Welcome to Rock Paper Scissors
+    </h1>
+    <a href="login.php">Please Log In</a>
+    <p>Attempt to go to <a href="game.php">game.php</a> without loggin in - it should fail with an error message.</p>
+</body>
+</html>
